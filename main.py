30ce7652948aabe8d0efb75ec15cf9549c4dd2c8
Carban2717 <165124910+Carban2717@users.noreply.github.com>
2024-08-31 22:33:02 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+import random
+import time
+from colorama import Fore, Style, init
+
+
+init(autoreset=True)
+
+def clear_screen():
+    """Ekranı temizler."""
+    os.system('clear') 
+
+def print_menu():
+    """Menüyü yazdırır."""
+    clear_screen()
+    print(Fore.BLUE + "
+   ____ ____      _    _____ _____ ___   ___  _     
+  / ___|  _ \    / \  |  ___|_   _/ _ \ / _ \| |    
+ | |  _| |_) |  / _ \ | |_    | || | | | | | | |    
+ | |_| |  _ <  / ___ \|  _|   | || |_| | |_| | |___ 
+  \____|_| \_\/_/   \_\_|     |_| \___/ \___/|_____|
+                                                    
+    ")
+    print(Fore.RED + "Developer: carbans2717")
+    print(Fore.GREEN + "[01] Çıkış")
+    print(Fore.GREEN + "[02] Random Gmail")
+
+def generate_random_gmails(count):
+    """Rastgele Gmail adresleri üretir."""
+    gmails = []
+    for _ in range(count):
+        username = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz1234567890', k=10))
+        gmails.append(f'{username}@gmail.com')
+    return gmails
+
+def main():
+    while True:
+        print_menu()
+        choice = input(Fore.GREEN + "Seçiminizi yapın (1 veya 2): ")
+
+        if choice == '1':
+            clear_screen()
+            print(Fore.GREEN + "Çıkış yapılıyor...")
+            time.sleep(1)
+            break
+        elif choice == '2':
+            clear_screen()
+            print(Fore.GREEN + "50 adet Random Gmail adresi:")
+            gmails = generate_random_gmails(50)
+            for gmail in gmails:
+                print(gmail)
+            input(Fore.GREEN + "Devam etmek için bir tuşa basın...")
+        else:
+            clear_screen()
+            print(Fore.RED + "Geçersiz seçim. Lütfen tekrar deneyin.")
+            time.sleep(1)
+
+if __name__ == "__main__":
+    main()
